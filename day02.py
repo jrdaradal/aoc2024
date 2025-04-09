709dd14012e6c5f08d13bcd15af95a15dcca0b66
Roi Daradal <roidaradal@gmail.com>
2025-04-09 10:46:32 +0800

    v0.2.2 - Day 02.2

--- day02.py
+++ day02.py
@@ -2,6 +2,7 @@
 # John Roy Daradal 
 
 # SolutionA: 490
+# SolutionB: 536
 
 from utils import * 
 
@@ -16,13 +17,33 @@ def day02A():
             count += 1 
     print(count)
 
-def isSafe(line):
+def day02B():
+    full = True 
+    count = 0 
+    for numbers in input02(full):
+        if isSafeV2(numbers):
+            count += 1 
+    print(count)
+
+def isSafe(numbers: list[int]) -> bool:
     diffs = []
-    for i in range(1,len(line)):
-        diffs.append(line[i]-line[i-1])
+    for i in range(1,len(numbers)):
+        diffs.append(numbers[i]-numbers[i-1])
     safeInc = all(1 <= d and d <= 3 for d in diffs)
     safeDec = all(-3 <=d  and d <= -1 for d in diffs)
     return safeInc or safeDec
 
+def isSafeV2(numbers: list[int]) -> bool:
+    remove = [None] + list(range(len(numbers)))
+    for idx in remove:
+        if idx is None:
+            numbers2 = numbers[:]
+        else:
+            numbers2 = numbers[:idx] + numbers[idx+1:]
+        if isSafe(numbers2):
+            return True 
+    return False
+
 if __name__ == '__main__':
     day02A()
+    day02B()
